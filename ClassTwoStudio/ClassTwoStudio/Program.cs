728ecda592f32e8f854f1fa2109e838bde576a9d
Parker Stanley <parker.stanley@SOMO-M-DC-005.local>
2020-10-05 15:04:25 -0400

    finished everything except the super bonus mission

--- ClassTwoStudio/ClassTwoStudio/Program.cs
+++ ClassTwoStudio/ClassTwoStudio/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ClassTwoStudio
 {
@@ -8,7 +9,11 @@ namespace ClassTwoStudio
     {
         static void Main(string[] args)
         {
-            string someString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            Console.WriteLine("Please input a string to retrieve character count:");
+            string userInput = Console.ReadLine().ToLower();
+            string someString = Regex.Replace(userInput, "[^A-Za-z0-9]", "");
+
+            //string someString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
             char[] charactersInString = someString.ToCharArray();
             Dictionary<char, double> characterCount = new Dictionary<char, double>();
 
@@ -23,7 +28,8 @@ namespace ClassTwoStudio
                 }
             }
 
-            Console.WriteLine(characterCount);
+            //Console.WriteLine(characterCount.ToString()); 
+            characterCount.Select(i => $"{i.Key}: {i.Value}").ToList().ForEach(Console.WriteLine);
         }
     }
 }
